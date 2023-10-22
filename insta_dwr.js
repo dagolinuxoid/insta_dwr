af0fbda6b4b94621cfc1981ef1824683a5feec84
artur_haurylkevich <daniel1kdago@gmail.com>
2023-10-22 22:11:50 +0300

    use clipbaordAPI
    refactor using clipboardAPI;
    assign Backspace as a key trigger;
    define additional variables;

--- insta_dwr.js
+++ insta_dwr.js
@@ -1,24 +1,37 @@
 /* Get urls of pictures on Instagram */
 const links = new Set();
+const triggerKey = 'Backspace';
+const target = '._aagv';
+
+const saveToClipboard = async (urls) => {
+	try {
+		await navigator.clipboard.writeText(urls);
+		console.log('URLs copied to clipoard');
+	} catch (err) {
+		console.error('Failed to copy: ', err);
+	}
+};
 
 window.addEventListener('keyup', event => {
-	if (event.key === 'End') {
+	if (event.key === triggerKey) {
 		getUrls().forEach(url => links.add(url));
-		console.log(`You got ::${links.size}:: urls so far`);
+		console.log(`You got ::${links.size}:: URLs so far`);
+		saveToClipboard([...links].join('\n'));
+	} else {
+		console.log(`HINT: press ${triggerKey} to grab URLs`)
 	}
 });
 
 function getUrls() {
 	const urls = [];
-	const rows = document.querySelectorAll('._aang');
+	const rows = document.querySelectorAll(target);
 	rows.forEach(row =>
 		row.querySelectorAll('img').forEach(img => urls.push(img.src))
 	);
 	return urls;
 }
 
-// call it once at the end and "Copy string contents" from devtools output
-[...links].join('\n');
-
 // save: for i in $(xclip -se c -o);do curl -# -O $i;done
-// view: xclip -se c -o|xargs mpv
+// make_playlist: xclip -se c -o > gallery.m3u
+// view: xclip -se c -o | xargs mpv
+// lazy_view: xclip -o|xargs mpv 
